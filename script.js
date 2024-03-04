610cf492e7662b481b7c4508754a7714c3676dcd
Christopher Cantu <chrischimi02@gmail.com>
2024-03-04 02:47:39 -0600

    website for my gf

--- script.js
+++ script.js
@@ -1,4 +1,17 @@
-const h2 = document.createElement("h2");
-h2.textContent = "This content added by JavaScript";
+document.addEventListener("DOMContentLoaded", function() {
+    const breedSelect = document.getElementById('breed-select');
+    const showImageButton = document.getElementById('show-image-btn');
+    const dogImage = document.getElementById('dog-image');
 
-document.querySelector("body").appendChild(h2);
+    breedSelect.addEventListener('change', function() {
+        const selectedBreed = breedSelect.value;
+        fetchBreedInfo(selectedBreed);
+    });
+
+    showImageButton.addEventListener('click', function() {
+        dogImage.src = 'https://i.redd.it/fozo8fwvcey71.jpg';
+        dogImage.style.display = 'block';
+    });
+});
+
+h2.textContent = "This content added by JavaScript";
